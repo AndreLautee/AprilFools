c065f0bd1b267ad9cc68aff82ea4339f93e8e3eb
AndreLautee <42164078+AndreLautee@users.noreply.github.com>
2021-04-26 22:17:40 +1000

    Allow basic note edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Table } from "react-bootstrap";
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  let data = [
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([
     {
       course: "Art 5th",
       grade: "75",
@@ -41,45 +42,132 @@ export default function App() {
       latest_submission: null,
       note: null
     }
-  ];
+  ]);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("A note was submitted: " + value);
+    //data[0].note = value;
+    const newData = [
+      {
+        course: "Art 5th",
+        grade: "75",
+        zeros: 0,
+        missing_assignments: 0,
+        last_access: "03/03/2021",
+        latest_submission: null,
+        note: value
+      },
+      {
+        course: "COUNSELOR EL",
+        grade: "None",
+        zeros: 0,
+        missing_assignments: 0,
+        last_access: "03/03/2021",
+        latest_submission: null,
+        note: null
+      },
+      {
+        course: "ELA",
+        grade: "26.67",
+        zeros: "4(show)",
+        missing_assignments: "3(show)",
+        last_access: "03/11/2021",
+        latest_submission: "03/08/2021",
+        note: null
+      },
+      {
+        course: "Homeroom 5th",
+        grade: "99",
+        zeros: 0,
+        missing_assignments: 0,
+        last_access: "03/03/2021",
+        latest_submission: null,
+        note: null
+      }
+    ];
+    setData(newData);
+  };
 
   return (
     <div className="App">
       <Container>
-        <Table bordered hover>
-          <thead>
-            <tr>
-              <th>Course</th>
-              <th>Grade</th>
-              <th>Zeros</th>
-              <th>Missing Assignments</th>
-              <th>Last Access</th>
-              <th>Latest Submission</th>
-              <th>Note</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((data) => {
-              return (
-                <tr key={data.course}>
-                  <td>{data.course}</td>
-                  <td>{data.grade}</td>
-                  <td>{data.zeros}</td>
-                  <td>${data.missing_assignments}</td>
-                  <td>${data.last_access}</td>
-                  <td>{data.latest_submission}</td>
-                  <td>{data.note}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-        <Notes></Notes>
+        <Tables data={data} />
+        <Notes handleSubmit={handleSubmit} handleChange={handleChange} />
       </Container>
     </div>
   );
 }
+function Tables(props) {
+  return (
+    <div>
+      <Table bordered hover>
+        <thead>
+          <tr>
+            <th>Course</th>
+            <th>Grade</th>
+            <th>Zeros</th>
+            <th>Missing Assignments</th>
+            <th>Last Access</th>
+            <th>Latest Submission</th>
+            <th>Note</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.data.map((data) => {
+            return (
+              console.log(data.note),
+              (
+                <Row
+                  key={data.course}
+                  course={data.course}
+                  grade={data.grade}
+                  zeros={data.zeros}
+                  missing_assignments={data.missing_assignments}
+                  last_access={data.last_access}
+                  latest_submission={data.latest_submission}
+                  note={data.note}
+                />
+              )
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+function Row(props) {
+  return (
+    <tr key={props.course}>
+      <td>{props.course}</td>
+      <td>{props.grade}</td>
+      <td>{props.zeros}</td>
+      <td>${props.missing_assignments}</td>
+      <td>${props.last_access}</td>
+      <td>{props.latest_submission}</td>
+      <td>{props.note}</td>
+    </tr>
+  );
+}
 
+function Notes(props) {
+  return (
+    <div id="noteArea">
+      <form onSubmit={props.handleSubmit}>
+        <textarea
+          type="text"
+          id="note"
+          name="note"
+          onChange={props.handleChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+/*
 class Notes extends React.Component {
   constructor(props) {
     super(props);
@@ -117,3 +205,4 @@ class Notes extends React.Component {
     );
   }
 }
+*/
